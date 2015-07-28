a6aeb754f51b6e2746d8fab3e318398aeeb278c0
Luccas Maso <luccas.no@gmail.com>
2015-07-27 23:01:03 -0300

    Update jquery.scraper.js

--- jquery.scraper.js
+++ jquery.scraper.js
@@ -2,12 +2,12 @@
 
   $.fn.scraper = function (options){
     this.each(function(){
-      var width = options.width || element.parent().width()
       var element = $(this);
+      var defaults = {width: element.parent().width()}
+      options = $.extend(defaults, options || {});
       var href = element.attr('href');
       $.ajax({
-
-        url: "http://scraper.luccasmaso.com/api/v1/oembed?url=" + encodeURIComponent(href) + "&width=" + width + "&autoplay=1"
+        url: "http://scraper.luccasmaso.com/api/v1/oembed?url=" + encodeURIComponent(href) + "&width=" + options.width + "&autoplay=1"
       }).done(function(response){
         if(response.type == "rich" || response.type == "video"){
           element.replaceWith(response.html);
