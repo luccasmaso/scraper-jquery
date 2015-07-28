31676bc18b0d720424ce9942c6737fac1b0f74a7
Luccas Maso <luccas.no@gmail.com>
2015-07-28 01:57:56 -0300

    Update jquery.scraper.js
    Adding https and optional callback param when done fetching

--- jquery.scraper.js
+++ jquery.scraper.js
@@ -7,10 +7,13 @@
       options = $.extend(defaults, options || {});
       var href = element.attr('href');
       $.ajax({
-        url: "http://scraper.luccasmaso.com/api/v1/oembed?url=" + encodeURIComponent(href) + "&width=" + options.width + "&autoplay=1"
+        url: "https://scraper.luccasmaso.com/api/v1/oembed?url=" + encodeURIComponent(href) + "&width=" + options.width + "&autoplay=1"
       }).done(function(response){
         if(response.type == "rich" || response.type == "video"){
           element.replaceWith(response.html);
+          if(options.done){
+            options.done.call();
+          }
         }
       });
     });
